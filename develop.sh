056fe7d512eeb560d0a83bc54c5d88d012ee6c9f
David, Jian <clipwww@gmail.com>
2020-02-09 15:36:45 +0800

    fix: 修正shellscript 😎

--- develop.sh
+++ develop.sh
@@ -3,11 +3,8 @@
 # abort on errors
 set -e
 
-# build
-npm run build
-
 # navigate into the build output directory
-cd build
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
